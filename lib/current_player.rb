8f86d39b0d92a88f5112889e8e0dfc7cce4569fc
morra.dianna <morra.dianna@gmail.com>
2020-06-21 19:45:51 +0000

    Automatically backed up by Learn

---
+++ lib/current_player.rb
@@ -0,0 +1,15 @@
+
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+def display_board(board)
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
+
+display_board(board)
+
+def turn_count(array)
+  arr.each do | board |
